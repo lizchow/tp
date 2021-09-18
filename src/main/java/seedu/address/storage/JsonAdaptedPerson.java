6ca1dbdcf21fe6b5088bcf16a023d3372bc2846d
elizabethchow1234@gmail.com <elizabethchow1234@gmail.com>
2021-09-18 15:34:36 +0800

    Update Ui

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -107,7 +107,7 @@ class JsonAdaptedPerson {
         final Address modelAddress = new Address(address);
 
         if (remark == null) {
-            throw new IllegalValueException((String.format(MISSING_FIELD_MESSAGE_FORMAT)));
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Remark.class.getSimpleName()));
         }
         final Remark modelRemark = new Remark(remark);
 
